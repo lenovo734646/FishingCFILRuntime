100c8bebd6abcaaf649260dcf85c0eecc4156efa
yuansan <11@11>
2022-05-09 11:33:18 +0800

    1111

---
+++ Hotfix/Games/SLWH/MyApp.cs
@@ -0,0 +1,22 @@
+﻿using Hotfix.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotfix.SLWH
+{
+	public class MyApp : AppBase
+	{
+		public ThisGameConfig conf = new ThisGameConfig();
+		public GameController theGame = new GameController();
+		public override void Start()
+		{
+			game = theGame;
+			conf.Init();
+			base.Start();
+		}
+	}
+}
